d74222ef139361a5a8bf3f8f0f4de47ea90fb580
Danpeo <danilvar4@gmail.com>
2024-05-14 16:42:22 +0400

    Make MathUtil static class

--- DVar.Geometry/MathUtil.cs
+++ DVar.Geometry/MathUtil.cs
@@ -1,6 +1,6 @@
 namespace DVar.Geometry;
 
-public class MathUtil
+public abstract class MathUtil
 {
     public static bool IsAlmostEqual(double lhs, double rhs, double tolerance = 0.0001) => 
         Math.Abs(lhs - rhs) < tolerance;
